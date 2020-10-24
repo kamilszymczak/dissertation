59b975cf13c81cc408ddaf58977cb943ed234292
Kamil <kamil_szymczak@hotmail.com>
2020-10-24 19:25:11 +0100

    Scraper: each url goes into own csv file + various improvements

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,8 +3,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-urls = ['https://www.studentcrowd.com/university-l1006588-s1008458-university_of_stirling-stirling']
-
 def score_conversion(score):
     score = int(score)
     if score <= 2:
@@ -14,7 +12,7 @@ def score_conversion(score):
     return 2
 
 
-def build_dataset(seed_url):
+def build_dataset(seed_url, separateFile=True):
     reviews_data = []
     
     for url in urls:
@@ -27,17 +25,26 @@ def build_dataset(seed_url):
 
             for review in soup.find_all('div', attrs={"itemprop": "review"})]   
 
-    
+        if separateFile:
+            #Making file name from studetcrowd url
+            dataset_name = url.split("-")
+            dataset_name = dataset_name[-2]
+            toCSV(reviews, dataset_name)
+
         reviews_data.extend(reviews)
+        
 
     return reviews_data
 
 
+def toCSV(dataset, filename='export_dataframe'):
+    df =  pd.DataFrame(dataset)
+    df = df[['review', 'score']]
+    df.to_csv (filename + '.csv', index = False, header=True, encoding='utf-8-sig')
+
+
+urls = ['https://www.studentcrowd.com/university-l1002555-s1008268-heriot_watt_university-edinburgh']
 dataset = build_dataset(urls)
 
-print(dataset)
 
-df =  pd.DataFrame(dataset)
-df = df[['review', 'score']]
-
-df.to_csv (r'C:\Users\Kamcio\Desktop\export_dataframe.csv', index = False, header=True)
+print("Export Completed!")
