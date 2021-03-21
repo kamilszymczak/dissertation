89021ca9b84b4a5e7b44287b1b971353e905a52a
Kamil <kamil_szymczak@hotmail.com>
2021-03-21 21:12:21 +0000

    helpfun: removed hard coded absolute file path

--- Scripts/helperfn.py
+++ Scripts/helperfn.py
@@ -3,7 +3,7 @@ import glob
 import re
 from nltk.corpus import stopwords
 
-def merge_datasets(folder_path):
+def merge_datasets(path):
     """Merging multiply csv datasets into one DataFrame
 
     :param folder_path: Relative path of folder containing all datasets 
@@ -11,7 +11,7 @@ def merge_datasets(folder_path):
     :return: Single DataFrame of all datasets
     :rtype: pandas.dataframe
     """
-    path = r'E:\GitHubProjects\dissertation\scraper\approved_datasets'
+    # path = r'E:\GitHubProjects\dissertation\scraper\approved_datasets'
     all_files = glob.glob(path + "/*.csv")
 
     li = []
@@ -72,7 +72,7 @@ def balance_dataset(df):
     # Put all negative reviews/class in a separate dataset.
     negative_df = shuffled_df.loc[shuffled_df['score'] == 0]
 
-    #Randomly select 108 postive reviews (majority class), as there are 108 negative reviews
+    #Randomly select negative_df size postive reviews (majority class)
     positive_df = shuffled_df.loc[shuffled_df['score'] == 1].sample(n=negative_df.shape[0])
 
     # Concatenate both dataframes again
