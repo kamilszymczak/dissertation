6a559bca8431ca74f2bc17fd3d9012b04cd1bb47
Kamil <kamil_szymczak@hotmail.com>
2021-04-07 14:08:07 +0100

    scraper: finalized

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-
 def score_conversion(score):
     score = int(score)
     if score <= 2:
@@ -11,18 +10,15 @@ def score_conversion(score):
         return 1
     return 2
 
-
 def build_dataset(seed_url, separateFile=True):
     reviews_data = []
-    
-    for url in urls:
 
+    for url in urls:
         data = requests.get(url)
         soup = BeautifulSoup(data.content, 'html.parser')
 
         reviews = [
             {'review': review.find('span', attrs={"itemprop": "reviewBody"}).string , 'score': score_conversion(str(review.find('div', {"class": "review-box__stars"}))[-9:-8])}
-
             for review in soup.find_all('div', attrs={"itemprop": "review"})]   
 
         if separateFile:
@@ -32,19 +28,14 @@ def build_dataset(seed_url, separateFile=True):
             toCSV(reviews, dataset_name)
 
         reviews_data.extend(reviews)
-        
-
     return reviews_data
-
 
 def toCSV(dataset, filename='export_dataframe'):
     df =  pd.DataFrame(dataset)
     df = df[['review', 'score']]
     df.to_csv (filename + '.csv', index = False, header=True, encoding='utf-8-sig')
 
-
 urls = ['https://www.studentcrowd.com/university-l1002555-s1008268-heriot_watt_university-edinburgh']
 dataset = build_dataset(urls)
-
 
 print("Export Completed!")
